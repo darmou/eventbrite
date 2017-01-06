2065b888077d831179555c9e5b2969c462df1073
Daryl Moulder <dmoulder@qtome.com>
2017-01-05 21:17:01 -0800

    add detail to category

--- lib/eventbrite/resources/category.rb
+++ lib/eventbrite/resources/category.rb
@@ -4,6 +4,11 @@ module Eventbrite
       "/categories"
     end
 
+    def self.detail(id, params={}, tokon=nil)
+      response, token = Eventbrite.request(:get, url + "/" + id.to_s, token, params)
+      Util.convert_to_eventbrite_object(response, token)
+    end
+
     def self.all(params={}, token=nil)
       response, token = Eventbrite.request(:get, url, token, params)
       Util.convert_to_eventbrite_object(response, token)
