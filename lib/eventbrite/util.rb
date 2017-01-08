5a6f4b1990d701a1a14908b7b0151c893229aed0
Daryl Moulder <dmoulder@qtome.com>
2017-01-07 18:45:02 -0800

    try ticket class again

--- lib/eventbrite/util.rb
+++ lib/eventbrite/util.rb
@@ -4,7 +4,7 @@ module Eventbrite
       @object_classes ||= {
         events: Event,
         categories: Category,
-        ticket_class: TicketClass,
+        ticket_classes: TicketClass,
         attendees: Attendee,
         orders: Order,
         discouns: Discount,
--- lib/eventbrite/version.rb
+++ lib/eventbrite/version.rb
@@ -1,3 +1,3 @@
 module Eventbrite
-  VERSION = "0.1.2"
+  VERSION = "0.1.3"
 end
